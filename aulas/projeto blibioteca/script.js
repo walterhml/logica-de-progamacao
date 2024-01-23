048e805a422e3fd502801cb26150d41cfd9c4766
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2024-01-23 20:33:51 -0300

    implementação da logica do material blibiografico

--- aulas/projeto blibioteca/script.js
+++ aulas/projeto blibioteca/script.js
@@ -1,21 +1,38 @@
 class MaterialBibliografico {
-constructor(titulo, autor) {
-    this.titulo = titulo;
-    this.autor = autor;
-    this.disponivel = true;
-}
+    constructor(titulo, autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+    }
+    realizarEmprestimo() {
+        if (this.disponivel) {
+            this.disponivel = false;
+            return true; //Emprestimo realizado com sucesso
+        } else {
+            return false; //Material já emprestado
+        }
+    }
+
+    realizarDevolucao() {
+        if (this.disponivel === false) {
+            this.disponivel = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 class Livro extends MaterialBibliografico {
-constructor(titulo, autor, genero){
-    super(titulo, autor);
-    this.genero = genero;
-}
+    constructor(titulo, autor, genero) {
+        super(titulo, autor);
+        this.genero = genero;
+    }
 }
 
 class Revista extends MaterialBibliografico {
-constructor(titulo, autor, genero, categoria){
-    super(titulo, autor);
-    this.categoria = categoria;
-}
+    constructor(titulo, autor, genero, categoria) {
+        super(titulo, autor);
+        this.categoria = categoria;
+    }
 }
