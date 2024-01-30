4a34193649525aae22dddd4609ee6cac1d08e90e
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2024-01-29 22:16:21 -0300

    Aula orientada a objetos

--- aulas/aula10/script.js
+++ aulas/aula10/script.js
@@ -17,6 +17,11 @@ Crie a função atacar
     - atacar, recebe como parametro um personaem/inimigo
     - so pode atacar alguém com vida
     - exibe o console.log informando qual a força do ataque e inimigo atacado
+
+Exercício:
+    - Crie um limite de vida máxima para o personagem
+    - Crie uma função de recuperar vida que recebe um total de pontos para recuperar
+    - A recuperação de via não pode fazer o personaem ir além da vida máxima
 */
 
 class Personagem {
@@ -27,6 +32,7 @@ class Personagem {
         this.posicao = posicao;
         this.vida = vida;
         this.vivo = vivo;
+        this.vidaMaxima = vida;
     }
 
     morrer() {
@@ -56,14 +62,24 @@ class Personagem {
             console.log("Não é possível atacar na condição de morto!");
         }
     }
+
+    recuperarVida(vidaRecebida, personagem){
+        personagem.vida += vidaRecebida;        
+
+        if(personagem.vida > personagem.vidaMaxima) {
+            personagem.vida = personagem.vidaMaxima;
+        }
+
+        console.log(`${this.nome} recuperou a vida de ${personagem.nome} um total de ${vidaRecebida}, ficando com ${personagem.vida} de vida`);
+    }
 }
 
 let personagem1 = new Personagem("Arthur", 10, 12, 100, 1, true);
-let personagem2 = new Personagem("Gendalf", 12, 8, 85, 1);
+let personagem2 = new Personagem("Gendalf", 2, 8, 85, 1);
 
 console.log(personagem1);
 console.log(personagem2);
 
 console.log(personagem1.atacar(personagem2));
 console.log(personagem2.atacar(personagem1));
-
+console.log(personagem1.recuperarVida(15, personagem2));
