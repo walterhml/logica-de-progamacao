022923ac7a8ffd49315ef58b520b5f18ebaa6245
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2024-01-29 21:20:29 -0300

    aula 10 orientada a objetos

--- aulas/aula10/script.js
+++ aulas/aula10/script.js
@@ -9,6 +9,14 @@ Crie a função tomarDano
     - tomar dano recebe um parametro, que é o valor do dano
     - diminui os pontos de vida do personagem, com base no dano tomado
     - verifica, se o dano for maior que os pontos de vida, personagem morre
+    - se os pontos de vida forem menor ou igual a zero, personagem morre
+    - ao morrer, personagem continua com 0 pontos de vida
+    - o personagem só pode tomar dano se estiver vivo
+
+Crie a função atacar
+    - atacar, recebe como parametro um personaem/inimigo
+    - so pode atacar alguém com vida
+    - exibe o console.log informando qual a força do ataque e inimigo atacado
 */
 
 class Personagem {
@@ -27,33 +35,35 @@ class Personagem {
     }
 
     tomarDano(quantidade) {
-        if (this.vivo) {
+        if (this.vivo) {            
             this.vida = this.vida - quantidade;
             console.log(`${this.nome} sofreu ${quantidade} de dano, e agora tem ${this.vida} de vida`)
+
             if (this.vida <= 0) {
                 this.vida = 0;
                 this.morrer();
-
             }
         } else {
-            console.log('${this.nome} não pode mais sofrer dano!');
+            console.log(`${this.nome} não pode mais sofrer dano!`);
         }
     }
 
-    atacar(inimigo){
-     if(inimigo.vivo){
-        inimigo.tomarDano(this.ataque);
-     }else{
-        console.log('Não é possivel atacar um inimigo morto!')
-     }
-
+    atacar(inimigo) {
+        if(inimigo.vivo && this.vivo) {
+            console.log(`${this.nome} atacou ${inimigo.nome} com força de ${this.ataque}`);
+            inimigo.tomarDano(this.ataque);
+        } else {
+            console.log("Não é possível atacar na condição de morto!");
+        }
     }
 }
 
 let personagem1 = new Personagem("Arthur", 10, 12, 100, 1, true);
+let personagem2 = new Personagem("Gendalf", 12, 8, 85, 1);
 
 console.log(personagem1);
-console.log(personagem1.tomarDano(1));
-console.log(personagem1.tomarDano(17));
-console.log(personagem1.tomarDano(90));
+console.log(personagem2);
+
+console.log(personagem1.atacar(personagem2));
+console.log(personagem2.atacar(personagem1));
 
