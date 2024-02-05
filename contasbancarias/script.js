2989ddec6378c729bbb627b451bd91e99a6ecf79
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2024-02-05 20:18:30 -0300

    Projeto contas bancarias diagrama e estrutura

--- contasbancarias/script.js
+++ contasbancarias/script.js
@@ -1,15 +1,50 @@
 class Cliente {
-
+    constructor(nome, cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
 }
 
 class Conta {
+    constructor(cliente, numero, saldo) {
+        this.cliente = cliente;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
 
+    sacar(valorParaSaque) {
+        return true;
+    }
+
+    depositar(valorParaDeposito) {
+        return true;
+    }
+
+    transferir(valorTransferencia, cliente) {
+        return true;
+    }
 }
 
-class ContaCorrente {
+class ContaCorrente extends Conta {
+    constructor(cliente, numero, saldo, limiteChequeEspecial) {
+        super(cliente, numero, saldo)
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
 
+    sacar(valorSaque) {
+        super(valorSaque);
+        return true;
+    }
 }
 
-class ContaPoupanca {
-    
+class ContaPoupanca extends Conta {
+    constructor(cliente, numero, saldo, taxaRendimento) {
+        super(cliente, numero, saldo);
+        this.taxaRendimento = taxaRendimento;
+    }
+
+
+    apicarRendimento() {
+
+    }
 }
